802ede3cff21978b64fd5ff84597008a68f65eed
Agzamov Rustam <agzamovr@gmail.com>
2016-07-09 23:43:17 +0500

    Implemented growing sum function with given order

---
+++ src/main/java/org/agzamovr/collectors/Sum.java
@@ -0,0 +1,103 @@
+package org.agzamovr.collectors;
+
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.*;
+import java.util.stream.Collector;
+
+import static java.util.stream.Collectors.toList;
+
+public class Sum<T> {
+    private final Comparator<T> comparator;
+
+    public Sum(Comparator<T> comparator) {
+        Objects.requireNonNull(comparator, "Comparator cannot be null");
+        this.comparator = comparator;
+    }
+
+    public List<Integer> summingInt(ToIntFunction<? super T> mapper,
+                                    List<T> list) {
+        return summingInt(mapper, list, toList());
+    }
+
+    public <A, R> R summingInt(ToIntFunction<? super T> mapper,
+                               List<T> list,
+                               Collector<Integer, A, R> downstream) {
+        list.sort(comparator);
+        int sum = 0;
+        Supplier<A> downstreamSupplier = downstream.supplier();
+        Function<A, R> finisher = downstream.finisher();
+        BiConsumer<A, Integer> downstreamAccumulator = downstream.accumulator();
+        A container = downstreamSupplier.get();
+        for (T item : list) {
+            sum = sum + mapper.applyAsInt(item);
+            downstreamAccumulator.accept(container, sum);
+        }
+        return finisher.apply(container);
+    }
+
+    public List<Long> summingLong(ToLongFunction<? super T> mapper,
+                                  List<T> list) {
+        return summingLong(mapper, list, toList());
+    }
+
+    public <A, R> R summingLong(ToLongFunction<? super T> mapper,
+                                List<T> list,
+                                Collector<Long, A, R> downstream) {
+        list.sort(comparator);
+        long sum = 0;
+        Supplier<A> downstreamSupplier = downstream.supplier();
+        Function<A, R> finisher = downstream.finisher();
+        BiConsumer<A, Long> downstreamAccumulator = downstream.accumulator();
+        A container = downstreamSupplier.get();
+        for (T item : list) {
+            sum = sum + mapper.applyAsLong(item);
+            downstreamAccumulator.accept(container, sum);
+        }
+        return finisher.apply(container);
+    }
+
+    public List<Double> summingDouble(ToDoubleFunction<? super T> mapper,
+                                      List<T> list) {
+        return summingDouble(mapper, list, toList());
+    }
+
+    public <A, R> R summingDouble(ToDoubleFunction<? super T> mapper,
+                                  List<T> list,
+                                  Collector<Double, A, R> downstream) {
+        list.sort(comparator);
+        double sum = 0;
+        Supplier<A> downstreamSupplier = downstream.supplier();
+        Function<A, R> finisher = downstream.finisher();
+        BiConsumer<A, Double> downstreamAccumulator = downstream.accumulator();
+        A container = downstreamSupplier.get();
+        for (T item : list) {
+            sum = sum + mapper.applyAsDouble(item);
+            downstreamAccumulator.accept(container, sum);
+        }
+        return finisher.apply(container);
+    }
+
+    public List<BigDecimal> summingBigDecimal(Function<? super T, BigDecimal> mapper,
+                                          List<T> list) {
+        return summingBigDecimal(mapper, list, toList());
+    }
+
+    public <A, R> R summingBigDecimal(Function<? super T, BigDecimal> mapper,
+                                      List<T> list,
+                                      Collector<BigDecimal, A, R> downstream) {
+        list.sort(comparator);
+        BigDecimal sum = BigDecimal.ZERO;
+        Supplier<A> downstreamSupplier = downstream.supplier();
+        Function<A, R> finisher = downstream.finisher();
+        BiConsumer<A, BigDecimal> downstreamAccumulator = downstream.accumulator();
+        A container = downstreamSupplier.get();
+        for (T item : list) {
+            sum = sum.add(mapper.apply(item));
+            downstreamAccumulator.accept(container, sum);
+        }
+        return finisher.apply(container);
+    }
+}
---
+++ src/test/java/org/agzamovr/collectors/SumTest.java
@@ -0,0 +1,105 @@
+package org.agzamovr.collectors;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toSet;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SumTest {
+
+    @Test
+    public void testSummingEmptyList() {
+        Sum<Integer> sum = new Sum<>(Integer::compare);
+        List<Integer> list = emptyList();
+
+        List<Integer> result = sum.summingInt(i -> i, list);
+
+        assertThat(result.isEmpty());
+    }
+
+    @Test
+    public void testSummingInt() {
+        Sum<Integer> sum = new Sum<>(Integer::compare);
+        List<Integer> list = asList(1, 2, 3);
+
+        List<Integer> result = sum.summingInt(i -> i, list);
+
+        assertThat(result).containsExactly(1, 3, 6);
+    }
+
+    @Test
+    public void testSummingIntWithSetCollector() {
+        Sum<Integer> sum = new Sum<>(Integer::compare);
+        List<Integer> list = asList(1, 2, 3);
+
+        Set<Integer> result = sum.summingInt(i -> i, list, toSet());
+
+        assertThat(result).containsExactly(1, 3, 6);
+    }
+
+    @Test
+    public void testSummingLong() {
+        Sum<Long> sum = new Sum<>(Long::compare);
+        List<Long> list = asList(1L, 2L, 3L);
+
+        List<Long> result = sum.summingLong(i -> i, list);
+
+        assertThat(result).containsExactly(1L, 3L, 6L);
+    }
+
+    @Test
+    public void testSummingLongWithSetCollector() {
+        Sum<Long> sum = new Sum<>(Long::compare);
+        List<Long> list = asList(1L, 2L, 3L);
+
+        Set<Long> result = sum.summingLong(i -> i, list, toSet());
+
+        assertThat(result).containsAll(asList(1L, 3L, 6L));
+    }
+
+    @Test
+    public void testSummingDouble() {
+        Sum<Double> sum = new Sum<>(Double::compare);
+        List<Double> list = asList(1.0, 2.1, 3.1);
+
+        List<Double> result = sum.summingDouble(d -> d, list);
+
+        assertThat(result).containsExactly(1.0, 3.1, 6.2);
+    }
+
+    @Test
+    public void testSummingDoubleWithSetCollector() {
+        Sum<Double> sum = new Sum<>(Double::compare);
+        List<Double> list = asList(1.0, 2.1, 3.1);
+
+        Set<Double> result = sum.summingDouble(i -> i, list, toSet());
+
+        assertThat(result).containsAll(asList(1.0, 3.1, 6.2));
+    }
+
+    @Test
+    public void testSummingBigDecimal() {
+        Sum<BigDecimal> sum = new Sum<>(BigDecimal::compareTo);
+        List<BigDecimal> list = asList(BigDecimal.ONE, new BigDecimal("2"), new BigDecimal("3"));
+
+        List<BigDecimal> result = sum.summingBigDecimal(d -> d, list);
+
+        assertThat(result).containsExactly(BigDecimal.ONE, new BigDecimal("3"), new BigDecimal("6"));
+    }
+
+    @Test
+    public void testSummingBigDecimalWithSetCollector() {
+        Sum<BigDecimal> sum = new Sum<>(BigDecimal::compareTo);
+        List<BigDecimal> list = asList(BigDecimal.ONE, new BigDecimal("2"), new BigDecimal("3"));
+
+        Set<BigDecimal> result = sum.summingBigDecimal(i -> i, list, toSet());
+
+        assertThat(result).containsAll(asList(BigDecimal.ONE, new BigDecimal("3"), new BigDecimal("6")));
+    }
+}
